a02ecaccc08af8f85f8b476a356e10be51444a4f
lifejireh <43215424+lifejireh@users.noreply.github.com>
2018-10-28 19:37:31 -0700

    Add files via upload
    Committing changes for BOADeckOfCardsChallenge project

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOADeckOfCardsChallenge
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<PokerCard> deck = new List<PokerCard>();
+            PokerCard[] initialDeck;
+                   
+            
+            CardDeck cd = new CardDeck();
+
+            // Loading a deck with 52 cards
+            initialDeck = cd.CreateDeck();
+
+            //Shuffling the deck of cards
+            PokerCard[] shuffleDeck = cd.Shuffle(initialDeck);
+            
+            //The following 4 lines of code can be uncommented if the user wants to display all cards of the shuffled deck 
+            //for (int i = 0; i < shuffleDeck.Length; i++)
+            //{
+            //    Console.WriteLine(shuffleDeck[i].Faces + " of " + shuffleDeck[i].Suit + '\n');
+            //}
+            
+             // Asking the user if they want to deal a card                      
+             cd.dealOneCard(shuffleDeck);
+                      
+            Console.ReadLine();
+
+        }
+    }
+}
